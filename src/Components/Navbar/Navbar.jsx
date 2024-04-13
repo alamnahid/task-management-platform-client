e98e86dd3a335f438258f88773dd83ed485a9438
alamnahid <nahidalam13765@gmail.com>
2024-04-13 22:26:52 +0600

    fix the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-
+import logo from "../../assets/logo/logo1.png"
 const Navbar = () => {
     const [open, setOpen] = useState(false)
 
@@ -20,51 +20,44 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ${open ? '' : 'hidden'}`}>
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/'>Home</NavLink></li>
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/about'>About</NavLink></li>
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/dashboard/dashboardhome'>Dashboard</NavLink></li>
+                            <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/'>Home</NavLink></li>
+                            <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/about'>About</NavLink></li>
+                            <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/dashboard/dashboardhome'>Dashboard</NavLink></li>
                         </ul>
                     </div>
-                    {/* logo and name of the website  */}
                     <div className="flex items-center gap-2">
-
-                        <h1 className="text-2xl hidden lg:block font-bold">Task<span className="text-[#8d4dbf]">Management</span></h1>
+                        <img src={logo} alt="" />
+                        <h1 className="text-2xl hidden lg:block font-bold">Task<span className="text-[#FD7B38]"> Manager</span></h1>
 
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className=" menu-horizontal px-1">
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/'>Home</NavLink></li>
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/about'>About</NavLink></li>
-                        <li><NavLink className={({ isActive }) => isActive ? 'btn btn-neutral text-base border-none bg-[#8d4dbf] text-white' : 'text-lg font-medium btn bg-white border-none shadow-none'} to='/dashboard/dashboardhome'>Dashboard</NavLink></li>
-
-
+                        <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/'>Home</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/about'>About Us</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? 'btn text-base border-none bg-[#FFF4EE] text-gray-700' : 'text-base font-medium btn bg-white border-none shadow-none text-gray-700'} to='/dashboard/dashboardhome'>Dashboard</NavLink></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
 
                     {
                         user ? <div className="flex gap-4 items-center">
-                            {user.photoURL ?  <img className="w-10 rounded-full" alt="Tailwind CSS Navbar component" src={user?.photoURL} />
-                               :
-                               <img className="w-10 rounded-full" alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />}
+                            {user.photoURL ? <img className="w-10 rounded-full" alt="Tailwind CSS Navbar component" src={user?.photoURL} />
+                            :
+                            <img className="w-10 rounded-full" alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />}
 
-                            <Link to="/"><button onClick={handlelogOut} className="btn btn-outline border-[#8d4dbf] w-full text-lg">Logout</button></Link>
+                            <Link to="/"><button onClick={handlelogOut} className="btn btn-outline border-[#FD7B38] w-full text-base text-gray-700">Logout</button></Link>
                         </div>
 
-
-                            :
-
-                            <div>
-                                <Link to="/login"><button className="btn btn-outline border-[] lg:w-[6rem] text-lg border-2">Signin</button></Link>
-                                <Link to="/signup"><button className="btn border-none bg-[#8d4dbf] lg:w-[6rem] text-lg text-white ml-4 btn-neutral">Register</button></Link>
-                            </div>
+                        :
+                        <div>
+                            <Link to="/login"><button className="btn btn-outline border-[] lg:w-[6rem] text-lg border-2">Signin</button></Link>
+                            <Link to="/signup"><button className="btn border-none bg-[#8d4dbf] lg:w-[6rem] text-lg text-white ml-4 btn-neutral">Register</button></Link>
+                        </div>
 
                     }
                 </div>
-
             </div >
-
         </div >
     );
 };
